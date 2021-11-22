3dd1961fb753a34acafe634f74d8e7a301d1c78c
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2021-11-22 21:38:43 +0100

    Arreglo de cut() en pipe
    Ahora los Tiles que se eliminan también cambian a estar vacíos

--- Practica2/Practica2_Moviles/Assets/Scripts/Pipe.cs
+++ Practica2/Practica2_Moviles/Assets/Scripts/Pipe.cs
@@ -18,29 +18,63 @@ namespace Flow
             _finished = false;
         }
 
+        /// <summary>
+        /// Aniade un nuevo tile a la tuberia
+        /// </summary>
+        /// <param name="newTile"></param>
         public void addTileToPipe(Tile newTile)
         {
+            //La tuberia no debe contener ya al tile
             if (!_pipe.Contains(newTile))
             {
+                //Si el nuevo tile es uno de los extremos y ya habia tiles en la tuberia, significa que se ha cerrado
                 if ((newTile == _secondTile || newTile == _firstTile) && _pipe.Count > 0)
                     _finished = true;
+
                 _pipe.Add(newTile);
             }
         }
 
+        /// <summary>
+        /// Elimina los tiles necesarios cuando la tuberia es cortada por otra
+        /// </summary>
+        /// <param name="cutted"></param>
         public void cut(Tile cutted)
         {
+            //Obtener indice del corte
             int where = _pipe.IndexOf(cutted);
+
+            //Si la tuberia no esta cerrada, se eliminan los tiles desde el corte hasta el final
             if (!_finished)
-                _pipe.RemoveRange(where, _pipe.Count - where - 1);
+                removeTilesRange(where, _pipe.Count - 1);
+            //Si esta cerrada, se corta el trazo con mas cantidad de tiles respecto al corte
             else
             {
-                if (where < _pipe.Count - where)
-                    _pipe.RemoveRange(0, where);
+                if (where < _pipe.Count - 1 - where)
+                    removeTilesRange(0, where);
                 else
-                    _pipe.RemoveRange(where, _pipe.Count - where - 1);
+                    removeTilesRange(where, _pipe.Count - 1);
             }
+
+            //La tuberia deja de estar cerrada
             _finished = false;
+        }
+
+        /// <summary>
+        /// Elimina y resetea los tiles de la tuberia desde un indice a otro [inicio, final)
+        /// </summary>
+        /// <param name="beginning"> Indice inicial </param>
+        /// <param name="end"> Indice final </param>
+        private void removeTilesRange(int beginning, int end)
+        {
+            //Reseteamos los tiles a vacios
+            for(int i = beginning; i < end; ++i)
+            {
+                _pipe[i].setTileType(Tile.TileType.voidTile); 
+            }
+
+            //Eliminamos de la lista los tiles dentro del rango
+            _pipe.RemoveRange(beginning, end - beginning);
         }
 
     }
