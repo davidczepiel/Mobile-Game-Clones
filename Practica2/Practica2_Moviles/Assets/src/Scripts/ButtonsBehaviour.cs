76cdfd66a733fee3ba74d2cda3d741980fe15df2
Sandra <samondra@ucm.es>
2021-12-04 19:07:07 +0100

    UI Creada
    - Posicionamiento de UI, no funcionalidad
    - Cambio de nombe de escena(para que deje de llamarse prueba)

---
+++ Practica2/Practica2_Moviles/Assets/src/Scripts/ButtonsBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsBehaviour : MonoBehaviour
+{
+    public static void GoToSelectMenu()
+    {
+        //
+    }
+
+    public static void GetHint()
+    {
+        //
+    }
+
+    public static void WatchVideo()
+    {
+        //
+    }
+
+    public static void RewindAction()
+    {
+        //
+    }
+
+    public static void NextLvl()
+    {
+        //
+    }
+
+    public static void PreviousLvl()
+    {
+        //
+    }
+
+    public static void RestartLvl()
+    {
+        //
+    }
+}
